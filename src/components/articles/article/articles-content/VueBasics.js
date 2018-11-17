2a9f700fd38c3e7e19b22d2a794ec78b85e37d75
eolculnamo2 <rbertram8@gmail.com>
2018-11-17 16:30:18 -0600

    first draft article 1

--- src/components/articles/article/articles-content/VueBasics.js
+++ src/components/articles/article/articles-content/VueBasics.js
@@ -4,13 +4,16 @@ import '../../articles.scss';
 const VueBasics = () => {
     return (
 <div>
+    <head>
+        <meta keywords="Vue VueJS Getting started with Vue VueJS for Beginners Vue Basics"></meta>
+        <meta description="A no non-sense guide to getting start with VueJS right away."></meta>
+    </head>
     <div className="article-title-wrap">
         <h1>VueJS Basics: The Vue Instance and The Basics of Components</h1>
         <h4>Rob Bertram</h4>
     </div>
-    <p>
-        The goal of this article is to give a brief introduction to the key features of VueJS. I do not cover set up with Vue. For this, I recommend using the [LINK] Vue CDN or [LINK] Vue CLI to get started.
-    </p>
+    <p>The goal of this article is to give a brief introduction to the key features of VueJS. If you're like me, you want the very basics of how to get started with a technology so that you can begin building and experimenting right away (its the best way to learn). This tutorial offers you a simple example of the core basics of Vue without spending time on parts that are not essential to building your first app.</p>
+    <p>For the sake of brevity, I do not cover set up with Vue. For this, I recommend using the <a href="https://vuejs.org/v2/guide/" target="_blank">Vue CDN or Vue CLI to get started</a>. Alternatively, you can skip this for now and simply <a href="https://codepen.io/eolculnamo2/pen/ZmJMPo" target="_blank">use the pen</a> below to practice before learning to set up your environment. This is a good option if you just want to see what Vue is about. If you love it (a lot of developers do), then go back later and learn the set up.</p>
     <p>    
         This article will first cover the Vue Instance and then how to extend much of its functionality into manageable components. 
     </p>
@@ -18,6 +21,10 @@ const VueBasics = () => {
     <img className="article-vue" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/2000px-Vue.js_Logo.svg.png" />
     
     <h3>The Vue Instance</h3>
+
+    <p>Throughout this section, I encourage you to use this pen as a reference to get you started. It has examples of all three of the topics we discuss: Data, Methods, and Computed values. By the end of this section, you should be familiar with how to use these three features. I encourage you to fork the pen and start practiciing/getting used to the features that Vue offers. When looking at this example, pay close attention to how the variables/functions on the HTML page interact with the values on the JS page.</p>
+
+    <iframe height='265' scrolling='no' title='VueJS Basics' src='//codepen.io/eolculnamo2/embed/ZmJMPo/?height=265&theme-id=dark&default-tab=js,result' frameBorder='no' allowTransparency='true' allowFullScreen='true' style={{width: "100%;"}}>See the Pen <a href='https://codepen.io/eolculnamo2/pen/ZmJMPo/'>VueJS Basics</a> by Rob (<a href='https://codepen.io/eolculnamo2'>@eolculnamo2</a>) on <a href='https://codepen.io'>CodePen</a>.</iframe>
     <p>
 	    The Vue Instance is where VueJS starts on your application. Instantiating Vue is simple enough. The first step is connecting Vue to your HTML so that Vue knows where to target its DOM manipulations. In our case, we will target an empty div with an id called “app”. After this, all we need to do is to instantiate the Vue Instance and tell it we want to target the div with an id “app”. To do this, we use the key ‘el’ (as in element) and tell it we want to target “#app”.
     </p>
@@ -108,7 +115,7 @@ const VueBasics = () => {
 	    Above you’ll notice that we added a new key to our instance called ‘methods’. Within the methods object, I added a function called updateExample. To update the piece of our data called example, all you have to do is reference the data object with ‘this’ and then tell it which piece of data needs updated (which is example). So we would say this.example = “Whatever your update is”.
     </p>
     <p>
-	    We’ve merely defined the function, but now we need to call the function. To do this, we’ll simple add a [LINK] Vue Click Event to a button our HTML page. While the different events on page are not within the scope of this article, I encourage you to read more about them [LINK] here when you have a chance. Below, we simply use v-on:click to tell Vue to call the updateExample() function when the user clicks the button. 
+	    We’ve merely defined the function, but now we need to call the function. To do this, we’ll simple add a <a href="https://vuejs.org/v2/guide/events.html" target="_blank">Vue Click Event</a> to a button our HTML page. While the different events on page are not within the scope of this article, I encourage you to read more about them <a href="https://vuejs.org/v2/guide/events.html" target="_blank">here</a> when you have a chance. Below, we simply use v-on:click to tell Vue to call the updateExample() function when the user clicks the button. 
     </p>
 <div className="article-code-block">
     <div>&lt;div id=”app”&gt;</div>
@@ -197,6 +204,94 @@ const VueBasics = () => {
 
     <p>
         This computed value is reactive which means that if something updates one of the two numbers, the computed value ‘total’ will reflect this change. For example, suppose you make a button with a function (in your methods object) which adds one to either of the numbers. As soon as the number is updated, your computed value will also update and the total on the screen will change to its updated value.  
+    </p>
+
+    <h3>Vue Components</h3>
+    
+    <p>The Vue Instance is a great place to start learn VueJS. However, its use in most projects is to define some global variables and functions and to facilitate the link between HTML and Vue Components. Before we go any further, it is important for you to know that moving from learning with the Vue Instance to working with Vue Components is not that hard. Vue components share many of the same options as the Vue Instance. For the sake of this section, we will again use data, methods, and computed values except in the Vue Component rather than the Vue Instance.  I’ve also broken down the process of bringing using Vue Components into simple steps.</p>
+    
+    <p>Before we get started, go ahead and open this codesandbox project. It was created with the default Vue CLI project with small modifications to make it more relevant to this article. Most of the work in this is from the original CLI. Use this as a reference to make your learning easier. When you're ready, for this project and start building your own Vue App with Vue Components. Go ahead and click the hamburger menu in the upper left hand corner of the iframe below to view the file system. The important files to us are index.html, main.js, and App.vue. Also pay attention the HelloWorld component and how it is called/used in App.js</p>
+    <iframe src="https://codesandbox.io/embed/50njmzy69p" style={{width: '100%;'}} sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
+
+    <br/><br/><em>Step 1 Setting up the Vue Instance</em><br/>
+
+    <p>Like before, we will start with an empty div and the Vue Instance. Vue Components cannot get to the DOM without the Vue Instance first. Notice in the example below that our instance now uses components and render. We will get to these soon, but for now just know that we will list the components we want to use in the components object and the render function will choose the component we want to render. Generally in the Vue Instance, there will be one component in components and it will be the components to be rendered in the render function. In this case, we will use the component App which we have not built yet.</p>
+
+    <div className="article-code-block">
+        <em>index.html</em><br/>
+        <div>&lt;div id=”app”&gt;</div>
+        <div>&lt;/div&gt;</div>
+        <br/>
+        <em>main.js -- The Vue Instance</em><br/>
+        <div>Import Vue from ‘vue’;</div>
+        <div>Import App from ‘./App.vue’;</div><br/>
+        <div>//This registers the component globally.</div>
+        <div>Vue.component("App",App);</div>
+        <div>{'new Vue({'}</div>
+                <div className="indent-1">el: “#app”,</div>
+        <div className="indent-1">{'components: {'}</div>
+            <div className="indent-2">App</div>
+        <div className="indent-1">},</div>
+        <div className="indent-1">render: h => h(App)</div>
+        <div>});</div>
+    </div>
+
+    <br/><br/><em>Step 2 Building the App Component</em><br/>
+    <p>Next we will construct the component in a .vue file. Vue files make front end development simple. They bring together HTML, JS, and CSS. For our App component, we will simply have template tags for our HTML, script tags for our JavaScript, and style tags for our CSS. A few important things to notice. 1) we have to include the name of the component in the script tags, 2) the data property returns an object whereas the instance does not (it won’t work without it in the component), and 3) we wrap the template in div tags to prevent the component from breaking.</p>
+
+    <div className="article-code-block">
+        <em>App.vue</em><br/>
+        <div>&lt;template&gt;</div>
+        <div className="indent-1">&lt;div class="msg-wrap"&gt;</div>
+            <div className="indent-2">{'{{ msg }}'}</div>
+        <div className="indent-1">&lt;/div&gt;</div>
+        <div>&lt;/template&gt;</div>
+        <div>&lt;script&gt;</div>
+        <div className="indent-1">//This is where you can import other components and dependencies. See App.vue in codesandbox for example.</div>
+        <div className="indent-1">{'export default {'}</div>
+            <div className="indent-2">{'data: function(){'}</div>
+                <div className="indent-3">msg: "Hello Vue"</div>
+            <div className="indent-2">{'},'}</div>
+            <div className="indent-2">{'methods:{'}</div>
+                <div className="indent-3">//This is where methods go just like on the Vue Instance</div>
+            <div className="indent-2">{'},'}</div>
+            <div className="indent-2">{'computed: {'}</div>
+                <div className="indent-3">//This is where computed values go just like on the Vue Instance</div>
+            <div className="indent-2">{'}'}</div>
+        <div className="indent-1">{'}'}</div>
+        <div>&lt;/script&gt;</div>
+        <div>&lt;style&gt;</div>
+        <div className="indent-1">{'.msg-wrap {'}</div>
+            <div className="indent-2">color: red;</div>
+        <div className="indent-1">{'.}'}</div>
+        <div>&lt;/style&gt;</div>
+    </div>
+
+    <br/><br/><em>Step 3 Referencing the component on the HTML page</em><br/>
+    <p>If you’ve come this far, congratulations. The hardest parts are over. We can now test our changes by putting the component on our index.html to file to render. Your index.html file will look like this.</p>
+
+    <div className="article-code-block">
+        <em>index.html</em><br/>
+        <div>&lt;div id=”app”&gt;</div>
+            <div className="indent-1">&lt;App /&gt;</div>
+        <div>&lt;/div&gt;</div>
+    </div>
+
+    <p>I highly recommend looking at the codesandbox example above to help get you started. Practice these basics and you’ll be much more comfortable with Vue in no time at all.</p>
+
+    <h3>Closing</h3>
+    <p>These are the basics for Vue. There are many other things to learn. Go build a simple app like a to do list. You now have the tools to do it! Once you are comfortable with these basics, you should go and research such items as:</p>
+    
+    <ul>
+        <li><a href="https://vuejs.org/v2/guide/list.html" target="_blank">Iteration with v-for</a></li>
+        <li><a href="https://vuejs.org/v2/guide/components.html" target="_blank">Nesting Components</a></li>
+        <li><a href="https://medium.com/@sky790312/about-vue-2-parent-to-child-props-af3b5bb59829" target="_blank">Emitting and Props</a></li>
+        <li><a href="https://vuejs.org/v2/guide/events.html" target="_blank">Vue inline events</a></li>
+        <li><a href="https://vuejs.org/v2/guide/class-and-style.html" target="_blank">Conditional Classes and Styling</a></li>
+    </ul>
+
+    <p>
+        Overall, take it slow. It is important to understand enough at first to get something working rather than trying to learn too much at once. Good luck with Vue!
     </p>
 </div>
     )
